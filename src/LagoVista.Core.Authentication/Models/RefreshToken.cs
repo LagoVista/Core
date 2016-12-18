e7e6fd486bf9844ff8608791136c6a325633b2ae
Kevin D. Wolf <kevinw@software-logistics.com>
2016-12-18 13:02:32 -0500

    added refresh token

---
+++ src/LagoVista.Core.Authentication/Models/RefreshToken.cs
@@ -0,0 +1,24 @@
+﻿using LagoVista.Core.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LagoVista.Core.Authentication.Models
+{
+    [EntityDescription(Name: "Refresh Token", Domain: Domains.AuthenticationDomain)]
+    public class RefreshToken
+    {
+        public string Subject { get; set; }
+        public string ClientId { get; set; }
+        public String IssuedUtc { get; set; }
+        public String ExpiresUtc { get; set; }
+        public string ProtectedTicket { get; set; }
+        public bool Enabled { get; set; }
+
+        public RefreshToken()
+        {
+            Enabled = true;
+        }
+    }
+}
