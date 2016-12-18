6554ecbc02a3e1e5adde0ca8ec0baecbce6edfd8
Kevin D. Wolf <kevinw@software-logistics.com>
2016-12-18 10:25:48 -0500

    add versioninfo class, add to domain descriptor

--- src/LagoVista.Core/Attributes/DomainDescriptionAttribute.cs
+++ src/LagoVista.Core/Attributes/DomainDescriptionAttribute.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LagoVista.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,15 +12,22 @@ namespace LagoVista.Core.Attributes
         private String _name;
         private String _description;
         private String _key;
+        private VersionInfo _currentVersion;
+        private List<VersionInfo> _versionHistory;
 
-        public DomainDescriptionAttribute(String Name = "", String Description = "", String Key = "")
+        public DomainDescriptionAttribute(String Name = "", String Description = "", String Key = "", VersionInfo currentVersion = null, List<VersionInfo> versionHistory = null)
         {
             _name = Name;
             _description = Description;
+            _key = Key;
+            _currentVersion = currentVersion;
+            _versionHistory = versionHistory;
         }
 
         public String Name { get; private set; }
         public String Key { get; private set; }
         public String Description { get; private set; }
+        public VersionInfo CurrentVersion { get; private set; }
+        public List<VersionInfo> VersionHistory { get; private set; }
     }
 }
---
+++ src/LagoVista.Core/Models/VersionInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LagoVista.Core.Models
+{
+    public class VersionInfo
+    {
+        public int Major { get; set; }
+        public int Minor { get; set; }
+        public int Revision { get; set; }
+        public long Build { get; set; }
+        public DateTime DateStamp { get; set; }
+        public String ReleaseNotes { get; set; }
+    }
+}
