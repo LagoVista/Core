f8f18ba15405eb15827b6ee812f6df8eb864dd78
Kevin D. Wolf <kevinw@software-logistics.com>
2017-01-26 19:04:52 -0500

    add ISerialPort, Add Methods for Popup for simple prompts

--- src/LagoVista.Core/PlatformSupport/IPopupServices.cs
+++ src/LagoVista.Core/PlatformSupport/IPopupServices.cs
@@ -12,5 +12,13 @@ namespace LagoVista.Core.PlatformSupport
 
         Task ShowAsync(String title, String message);
         Task ShowAsync(String message);
+
+        Task<String> ShowOpenFileAsync(string previousDirectory, string fileMask = "");
+
+        Task<String> ShowSaveFileAsync(string previousDirectory, string fileMask = "");
+
+        Task<String> PromptForStringAsync(String label, String defaultvalue = "");
+        Task<int> PromptForIntAsync(String label, int defaultvalue = 0);
+        Task<decimal> PromptForDecimalAsync(String label, decimal defaultvalue = 0);
     }
 }
---
+++ src/LagoVista.Core/PlatformSupport/ISerialPort.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LagoVista.Core.PlatformSupport
+{
+    public interface ISerialPort : IDisposable
+    {
+        Task<bool> OpenAsync(String portName, int baudRate);
+        Task<Stream> GetStreamAsync();
+        bool IsConnected { get; }
+        Task CloseAsync();
+    }
+}
