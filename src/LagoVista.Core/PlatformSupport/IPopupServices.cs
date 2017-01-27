5fc83605a73236d6558b973317d3dec6ff9287d9
Kevin D. Wolf <kevinw@software-logistics.com>
2017-01-27 06:04:18 -0500

    change signature on ShowSaveFile

--- src/LagoVista.Core/PlatformSupport/IPopupServices.cs
+++ src/LagoVista.Core/PlatformSupport/IPopupServices.cs
@@ -13,9 +13,9 @@ namespace LagoVista.Core.PlatformSupport
         Task ShowAsync(String title, String message);
         Task ShowAsync(String message);
 
-        Task<String> ShowOpenFileAsync(string previousDirectory, string fileMask = "");
+        Task<String> ShowOpenFileAsync(string fileMask = "");
 
-        Task<String> ShowSaveFileAsync(string previousDirectory, string fileMask = "");
+        Task<String> ShowSaveFileAsync(string fileMask = "", string defaultFileName = "");
 
         Task<String> PromptForStringAsync(String label, String defaultvalue = "");
         Task<int> PromptForIntAsync(String label, int defaultvalue = 0);
