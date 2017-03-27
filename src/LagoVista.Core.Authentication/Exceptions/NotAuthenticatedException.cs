149d795544823a79f42cf97fae4a6ef1cee053c8
Kevin D. Wolf <kevinw@software-logistics.com>
2017-03-27 08:55:22 -0400

    add auth models and exceptions

---
+++ src/LagoVista.Core.Authentication/Exceptions/NotAuthenticatedException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LagoVista.Core.Authentication.Exceptions
+{
+    public class NotAuthenticatedException : Exception
+    {
+    }
+}
---
+++ src/LagoVista.Core.Authentication/Exceptions/NotAuthorizedException.cs
@@ -0,0 +1,17 @@
+﻿using LagoVista.Core.Authentication.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LagoVista.Core.Authentication.Exceptions
+{
+    public class NotAuthorizedException : Exception
+    {
+        public NotAuthorizedException(AuthorizationResponse response)
+        {
+            AuthorizationResponse = response;
+        }
+
+        public AuthorizationResponse AuthorizationResponse { get; private set; }
+    }
+}
