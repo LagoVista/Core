60f322b4f8ceafde6d830c018cae644f210d7524
Kevin D. Wolf <kevinw@software-logistics.com>
2017-03-31 14:36:44 -0400

    Added Authorization to Core

---
+++ src/LagoVista.Core/Exceptions/NotAuthorizedException.cs
@@ -0,0 +1,15 @@
+﻿using LagoVista.Core.Models;
+using System;
+
+namespace LagoVista.Core.Authentication.Exceptions
+{
+    public class NotAuthorizedException : Exception
+    {
+        public NotAuthorizedException(AuthorizeResult response)
+        {
+            AuthorizationResponse = response;
+        }
+
+        public AuthorizeResult AuthorizationResponse { get; private set; }
+    }
+}
