f94ab9151eb5f918082577c0ad54fb3d3a840f71
Kevin D. Wolf <kevinw@software-logistics.com>
2017-06-08 11:43:24 -0400

    day of logging

--- src/LagoVista.Core/PlatformSupport/ILogger.cs
+++ src/LagoVista.Core/PlatformSupport/ILogger.cs
@@ -18,6 +18,10 @@ namespace LagoVista.Core.PlatformSupport
 
     public interface ILogger
     {
+        TimedEvent StartTimedEvent(string area, string description);
+        
+        void EndTimedEvent(TimedEvent evt);
+
         void SetUserId(string userId);
         void SetKeys(params KeyValuePair<String, String>[] args);
 
@@ -26,5 +30,21 @@ namespace LagoVista.Core.PlatformSupport
         void LogException(String area, Exception ex, params KeyValuePair<String, String>[] args);
 
         void TrackEvent(string message, Dictionary<string, string> parameters);
+    }
+
+    public class TimedEvent
+    {
+        public TimedEvent(string area, string description)
+        {
+            StartTime = DateTime.Now;
+            Area = area;
+            Description = description;
+        }
+
+        public DateTime StartTime { get; private set; }
+
+        public string Area { get; private set; }
+
+        public string Description { get; private set; }
     }
 }
