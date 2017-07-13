212dee570f98ef295c69729d497f36c41c871317
Kevin D Wolf <kevin@DESKTOP-R4064KP.(none)>
2017-07-13 08:32:26 -0400

    fix app instance manager bug

---
+++ src/LagoVista.Core/Extensions/InvokeResultExtensions.cs
@@ -0,0 +1,20 @@
+﻿using LagoVista.Core.Validation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LagoVista.Core
+{
+    public static class InvokeResultExtensions
+    {
+        public static KeyValuePair<string, string> ToKVP(this string value, string key)
+        {
+            return new KeyValuePair<string, string>(key, value);
+        }
+
+        public static InvokeResult ToInvokeResult(this string error)
+        {
+            return InvokeResult.FromErrors(new ErrorMessage(error));
+        }
+    }
+}
