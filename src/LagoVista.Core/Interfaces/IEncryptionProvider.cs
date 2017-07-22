7311dc1a982cd734f79f3facf7644ae6732ee50c
Kevin D Wolf <kevin@DESKTOP-R4064KP.(none)>
2017-07-22 10:11:54 -0400

    Add Secure Storage Project

---
+++ src/LagoVista.Core/Interfaces/IEncryptionProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LagoVista.Core.Interfaces
+{
+    public interface IEncryptionProvider
+    {
+        string Encrypt(string plainTextString);
+        string Decrypt(string encryptedString); 
+    }
+}
---
+++ src/LagoVista.Core/Interfaces/ISecureStorage.cs
@@ -0,0 +1,40 @@
+﻿using LagoVista.Core.Validation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LagoVista.Core.Interfaces
+{
+    public interface ISecureStorage
+    {
+        /// <summary>
+        /// Add a secret with a previously generated id
+        /// </summary>
+        /// <param name="id">Id to store secret</param>
+        /// <param name="value">Secret</param>
+        /// <returns>Id as passed in</returns>
+        Task<InvokeResult<string>> AddSecretAsync(string id, string value);
+
+        /// <summary>
+        /// Add a secret and generate and id
+        /// </summary>
+        /// <param name="value">Secret</param>
+        /// <returns>Generated Id</returns>
+        Task<InvokeResult<string>> AddSecretAsync(string value);
+
+        /// <summary>
+        /// Returns a Secret
+        /// </summary>
+        /// <param name="id">ID used for secret</param>
+        /// <returns>plain text value for secret</returns>
+        Task<InvokeResult<string>> GetSecretAsync(string id);
+
+        /// <summary>
+        /// Removes a secret
+        /// </summary>
+        /// <param name="id">ID of Secret to remove</param>
+        /// <returns></returns>
+        Task<InvokeResult> RemoveSecretAsync(string id);
+    }
+}
