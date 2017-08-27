cbcf12c66914b8c25e7a05dfe0fb566ee485d811
Kevin D. Wolf <kevinw@software-logistics.com>
2017-08-27 17:41:29 -0400

    significant progress on survey player

---
+++ src/LagoVista.Core/Extensions/EnumHelpers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Text;
+using LagoVista.Core.Attributes;
+
+namespace LagoVista.Core
+{
+    public static class EnumHelpers
+    {
+        public static string GetEnumLabel<TEnum>(this string value)
+        {
+            if (typeof(TEnum).GetTypeInfo().IsEnum)
+            {
+                var valueMember = typeof(TEnum).GetTypeInfo().DeclaredMembers.Where(mbr => mbr.Name == value.ToString()).FirstOrDefault();
+
+                var enumValues = Enum.GetValues(typeof(TEnum));
+                var len = enumValues.GetLength(0);
+                for (var idx = 0; idx <len; idx++)
+                {
+                    var enumValue = enumValues.GetValue(idx);
+                    var enumMember = typeof(TEnum).GetTypeInfo().DeclaredMembers.Where(mbr => mbr.Name == enumValue.ToString()).FirstOrDefault();
+                    var enumAttr = enumMember.GetCustomAttribute<EnumLabelAttribute>();
+                    if (enumAttr != null)
+                    {
+                        if (enumAttr.Key == value)
+                        {
+                            var labelProperty = enumAttr.ResourceType.GetTypeInfo().GetDeclaredProperty(enumAttr.LabelResource);
+                            return (string)labelProperty.GetValue(labelProperty.DeclaringType, null);
+                        }
+                    }
+                }
+
+                return value;
+            }
+
+            throw new Exception("GetEnumLabel only works with Enums.");
+        }
+    }
+}
---
+++ tests/LagoVista.Core.Tests/Extensions/EnumToDisplayTests.cs
@@ -0,0 +1,38 @@
+﻿using LagoVista.Core.Attributes;
+using LagoVista.Core.Tests.Resources.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LagoVista.Core.Tests.Extensions
+{
+
+    [TestClass]
+    public class EnumToDisplayTests
+    {
+        public enum SampleEnum
+        {
+            [EnumLabel("enum1", ValidationResources.Names.EnumOne, typeof(ValidationResources))]
+            Enum1,
+            [EnumLabel("enum2", ValidationResources.Names.EnumTwo, typeof(ValidationResources))]
+            Enum2,
+            [EnumLabel("enum3", ValidationResources.Names.EnumThree, typeof(ValidationResources))]
+            Enum3,
+            [EnumLabel("enum4", ValidationResources.Names.EnumFour, typeof(ValidationResources))]
+            Enum4,
+            [EnumLabel("enum5", ValidationResources.Names.EnumFive, typeof(ValidationResources))]
+            Enum5,
+        }
+
+
+        [TestMethod]
+        public void ResolveName()
+        {
+            Assert.AreEqual("Five","enum5".GetEnumLabel<SampleEnum>());
+        }
+
+    }
+}
