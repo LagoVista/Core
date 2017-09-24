0ad9e7b313d590d47470a625f9b58aa443a76611
Kevin D. Wolf <kevinw@software-logistics.com>
2017-09-24 15:12:00 -0400

    unit set/state set validation tests, UI updates, about 80% ready

---
+++ src/LagoVista.Core/Interfaces/ILockable.cs
@@ -0,0 +1,16 @@
+﻿using LagoVista.Core.Models;
+using System;
+
+namespace LagoVista.Core.Interfaces
+{
+    /* Can be attached to an Entity that will allow it to be locked so it needs to be unlocked by an org admin before it can be edited */
+    public interface ILockable
+    {
+        bool IsLocked { get; set; }
+
+        EntityHeader LockedBy { get; set; }
+
+        String LockedDateStamp { get; set; }
+
+    }
+}
