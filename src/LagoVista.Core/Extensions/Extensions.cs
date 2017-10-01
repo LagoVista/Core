5ebcd28e52a84391bd6a78794bef0dc3925ccc5b
Kevin D. Wolf <kevinw@software-logistics.com>
2017-10-01 19:37:13 -0400

    listener updates

--- src/LagoVista.Core/Extensions/Extensions.cs
+++ src/LagoVista.Core/Extensions/Extensions.cs
@@ -162,6 +162,42 @@ namespace LagoVista.Core
             }
         }
 
+        const char Base64Padding = '=';
+        static readonly HashSet<char> base64Table = new HashSet<char>{'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O',
+                                                                      'P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d',
+                                                                      'e','f','g','h','i','j','k','l','m','n','o','p','q','r','s',
+                                                                      't','u','v','w','x','y','z','0','1','2','3','4','5','6','7',
+                                                                      '8','9','+','/' };
+
+        public static bool IsBase64String(this string value)
+        {
+            value = value.Replace("\r", string.Empty).Replace("\n", string.Empty);
+
+            if (value.Length == 0 || (value.Length % 4) != 0)
+            {
+                return false;
+            }
+
+            var lengthNoPadding = value.Length;
+            value = value.TrimEnd(Base64Padding);
+            var lengthPadding = value.Length;
+
+            if ((lengthNoPadding - lengthPadding) > 2)
+            {
+                return false;
+            }
+
+            foreach (char c in value)
+            {
+                if (!base64Table.Contains(c))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
         public static string ToId(this Guid guid)
         {
             return guid.ToString().Replace("-", "").ToUpper();
