3c7af9a9daf1785143df32f4e49b2435aa36374b
Kevin D Wolf <kevin@DESKTOP-R4064KP.(none)>
2017-12-14 08:54:01 -0500

    create separate packages for models

--- src/LagoVista.Core/Models/InUseRecordData.cs
+++ src/LagoVista.Core/Models/InUseRecordData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,8 +7,10 @@ namespace LagoVista.Core.Models
 {
     public class InUseRecordData
     {
+        [JsonProperty("id")]
         public string Id { get; set; }
         public string Name { get; set; }
+        [JsonProperty("EntityType")]
         public string RecordType { get; set; }
         public string Uri { get; set; }
     }
