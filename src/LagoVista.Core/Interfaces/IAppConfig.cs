fa89a60e84b3bc757891a2c0b296025288f55d41
Kevin D. Wolf <kevindwolf@hotmail.com>
2017-12-22 17:45:45 -0500

    fixes/tweaks for models for client apps

--- src/LagoVista.Core/Interfaces/IAppConfig.cs
+++ src/LagoVista.Core/Interfaces/IAppConfig.cs
@@ -26,12 +26,24 @@ namespace LagoVista.Core.Interfaces
     {
         PlatformTypes PlatformType { get; }
         Environments Environment { get; }
+
         String WebAddress { get; }
+
+        String CompanyName { get; }
+
+        String CompanySiteLink { get; }
+
+
         String AppName { get; }
-        String AppId { get;  }
+        String AppId { get; }
+        String AppDescription { get; }
+        String TermsAndConditionsLink { get; }
+        String PrivacyStatementLink { get; }
         String ClientType { get; }
+
         String AppLogo { get; }
         String CompanyLogo { get; }
+
         bool EmitTestingCode { get; }
         VersionInfo Version { get; }
     }
--- src/LagoVista.Core/Models/VersionInfo.cs
+++ src/LagoVista.Core/Models/VersionInfo.cs
@@ -8,10 +8,15 @@ namespace LagoVista.Core.Models
     public class VersionInfo
     {
         public int Major { get; set; }
-        public int Minor { get; set; }
-        public int Revision { get; set; }
+        public int Minor { get; set; }        
         public long Build { get; set; }
+        public int Revision { get; set; }
         public DateTime DateStamp { get; set; }
         public String ReleaseNotes { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Major}.{Minor}.{Build}.{Revision}";
+        }
     }
 }
