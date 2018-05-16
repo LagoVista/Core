0b6fa57ab6af18782d1d9efbeeb4a1345e38ed92
Mark Lauter <mark.lauter@sumosoftware.com>
2018-05-16 16:40:25 -0400

    added IsRemoteable attribute for remote proxy support

---
+++ src/LagoVista.Core/Attributes/IsRemotableAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LagoVista.Core.Attributes
+{
+    /// <summary>
+    /// Usage: Interface contains remotable methods for run-time proxy generation. All methods will be used in remote proxy unless marked by ExcludeFromRemoteProxyAttribute.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Interface)]
+    public class IsRemotableAttribute : Attribute
+    {
+    }
+}
