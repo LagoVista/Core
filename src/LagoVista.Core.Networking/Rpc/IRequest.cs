9b5449bd3ce7590652d327ca55abd39edec72298
Mark Lauter <mark.lauter@sumosoftware.com>
2018-05-17 18:43:11 -0400

    moving rpc to async messaging

--- src/LagoVista.Core.Networking/Rpc/IRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LagoVista.Core.Networking.Rpc
-{
-    public interface IRequest : IMessage
-    {
-        void AddValue(string name, object value, bool ignoreDuplicates = false);
-        T GetValue<T>(string name);
-        object GetValue(string name);
-    }
-}
