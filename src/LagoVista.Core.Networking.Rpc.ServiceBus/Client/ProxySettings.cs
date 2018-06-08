29019303758fbc4b5a09db6c314201a97689a016
Mark Lauter <mark.lauter@sumosoftware.com>
2018-06-07 22:03:30 -0400

    removed networking rpc

--- src/LagoVista.Core.Networking.Rpc.ServiceBus/Client/ProxySettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LagoVista.Core.Networking.Rpc.ServiceBus.Client
-{
-    public sealed class ProxySettings
-    {
-        public string OrganizationId { get; set; }
-        public string InstanceId { get; set; }
-    }
-}
