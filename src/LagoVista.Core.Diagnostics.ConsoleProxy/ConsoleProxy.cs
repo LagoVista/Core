2fc1a601904a80519cda7c5322ff8006ad8c0d31
Mark Lauter <mark.lauter@sumosoftware.com>
2018-06-09 17:26:41 -0400

    added diagnostics console project

---
+++ src/LagoVista.Core.Diagnostics.ConsoleProxy/ConsoleProxy.cs
@@ -0,0 +1,71 @@
+﻿using LagoVista.Core;
+using Newtonsoft.Json;
+using System;
+using System.Reflection;
+
+namespace LagoVista.Diagnostics
+{
+    public sealed class ConsoleProxyProxyFactory
+    {
+        private readonly IConsoleWriter _console;
+
+        public ConsoleProxyProxyFactory(IConsoleWriter console)
+        {
+            _console = console ?? throw new ArgumentNullException(nameof(console));
+        }
+
+        public TInterface Create<TInterface>(TInterface instance) where TInterface : class
+        {
+//#if DEBUG
+            return ConsoleProxy<TInterface>.Create(instance, _console);
+//#else
+//            return instance;
+//#endif
+        }
+    }
+
+    public class ConsoleProxy<TInterface> : DispatchProxy where TInterface : class
+    {
+        private IConsoleWriter _console;
+        private TInterface _instance;
+
+        protected override object Invoke(MethodInfo targetMethod, object[] args)
+        {
+            object result = null;
+            _console.WriteLine($"-> {targetMethod.DeclaringType.FullName}.{targetMethod.Name}");
+            if (args != null && args.Length > 0)
+            {
+                try
+                {
+                    _console.WriteLine($"{targetMethod.DeclaringType.FullName}.{targetMethod.Name} args: {JsonConvert.SerializeObject(args)}");
+                }
+                catch(Exception ex)
+                {
+                    _console.WriteError($"{targetMethod.DeclaringType.FullName}.{targetMethod.Name} failed to convert args to json. Exception: {ex.GetType().Name}, {ex.Message}, {ex.Source}");
+                }
+            }
+            try
+            {
+
+                targetMethod.Invoke(_instance, args);
+                _console.WriteLine($"<- {targetMethod.DeclaringType.FullName}.{targetMethod.Name}");
+            }
+            catch (Exception ex)
+            {
+                _console.WriteError($"{targetMethod.DeclaringType.FullName}.{targetMethod.Name} Exception: {ex.GetType().Name}, {ex.Message}, {ex.Source}");
+                _console.WriteError($"{targetMethod.DeclaringType.FullName}.{targetMethod.Name} Stack Trace: {ex.StackTrace}");
+                throw;
+            }
+            return result;
+        }
+
+        public static TInterface Create(TInterface instance, IConsoleWriter console)
+        {
+            var result = Create<TInterface, ConsoleProxy<TInterface>>();
+            var proxy = (result as ConsoleProxy<TInterface>);
+            proxy._console = console ?? throw new ArgumentNullException(nameof(console));
+            proxy._instance = instance ?? throw new ArgumentNullException(nameof(instance));
+            return result;
+        }
+    }
+}
---
+++ src/LagoVista.Core.Diagnostics.ConsoleProxy/ConsoleWriter.cs
@@ -0,0 +1,40 @@
+using LagoVista.Core;
+using System;
+
+namespace LagoVista.Diagnostics
+{
+    public sealed class ConsoleWriter : IConsoleWriter
+    {
+        private readonly ConsoleColor _defaultForeground;
+        private readonly ConsoleColor _warningForeground;
+        private readonly ConsoleColor _errorForeground;
+
+        public ConsoleWriter(ConsoleColor defaultForeground = ConsoleColor.Green, ConsoleColor warningForeground = ConsoleColor.Yellow, ConsoleColor errorForeground = ConsoleColor.Red)
+        {
+            _defaultForeground = defaultForeground;
+            _warningForeground = warningForeground;
+            _errorForeground = errorForeground;
+        }
+
+        public void WriteError(string message)
+        {
+            Console.ForegroundColor = _errorForeground;
+            Console.WriteLine($"{DateTime.Now.ToLongTimeString()}  {message}");
+            Console.ResetColor();
+        }
+
+        public void WriteLine(string message)
+        {
+            Console.ForegroundColor = _defaultForeground;
+            Console.WriteLine($"{DateTime.Now.ToLongTimeString()}  {message}");
+            Console.ResetColor();
+        }
+
+        public void WriteWarning(string message)
+        {
+            Console.ForegroundColor = _warningForeground;
+            Console.WriteLine($"{DateTime.Now.ToLongTimeString()}  {message}");
+            Console.ResetColor();
+        }
+    }
+}
