5b10863a2559d84eb4d0346fdd2a9096ddad8992
Kevin D. Wolf <kevinw@software-logistics.com>
2018-07-29 16:42:27 -0400

    Add IChannel

---
+++ src/LagoVista.Core/PlatformSupport/IChannel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LagoVista.Core.PlatformSupport
+{
+    public interface IChannel
+    {
+        Task OpenAsync();
+        bool IsConnected { get; }
+        Task CloseAsync();
+
+        Task WriteAsync(string msg);
+        Task WriteAsync(byte[] buffer);
+        Task<int> ReadAsync(byte[] bufffer, int start, int size, CancellationToken token = default(CancellationToken));
+
+    }
+}
--- src/LagoVista.Core/PlatformSupport/ISerialPort.cs
+++ src/LagoVista.Core/PlatformSupport/ISerialPort.cs
@@ -8,16 +8,7 @@ using System.Threading.Tasks;
 
 namespace LagoVista.Core.PlatformSupport
 {
-    public interface ISerialPort : IDisposable
+    public interface ISerialPort : IChannel, IDisposable
     {
-        Task OpenAsync();
-        bool IsConnected { get; }
-        Task CloseAsync();
-
-        Task WriteAsync(string msg);
-        Task WriteAsync(byte[] buffer);
-        Task<int> ReadAsync(byte[] bufffer, int start, int size, CancellationToken token = default(CancellationToken));
-    }
-     
-
+    }    
 }
