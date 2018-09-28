b9e5604605975a433d7117bb80c80a171cea0dc6
Mark Lauter <ml.github@sumosoftware.com>
2018-09-28 12:05:26 -0400

    added test to debug our service bus address problem

--- tests/LagoVista.Core.Rpc.Tests/Server/RequestSeverTests.cs
+++ tests/LagoVista.Core.Rpc.Tests/Server/RequestSeverTests.cs
@@ -1,19 +1,20 @@
-﻿#define TEST_SERVICE_BUS
+﻿//#define TEST_SERVICE_BUS
 
-using LagoVista.Core.Models;
-using LagoVista.Core.Rpc.Messages;
-using Microsoft.Azure.ServiceBus;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+#if TEST_SERVICE_BUS
+using LagoVista.Core.Models;
+using LagoVista.Core.Rpc.Tests.Utils;
+using Microsoft.Azure.ServiceBus;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
+#endif
 
 namespace LagoVista.Core.Rpc.Tests.Server
 {
     [TestClass]
     public class RequestSeverTests
     {
+#if TEST_SERVICE_BUS
         [TestMethod]
         public async Task ServiceBus_TopicClient_SendAsync()
         {
@@ -22,7 +23,7 @@ namespace LagoVista.Core.Rpc.Tests.Server
                 AccountId = "localrequestbus-dev",
                 UserName = "SendAccessKey",
                 AccessKey = "B2bGyjZVtiNsgQ/BvjCqwtk9FgCYGdA7np99etWzHLc=",
-                ResourceName = "rcp_request"
+                ResourceName = "rpc_request"
             };
             var _destinationEntityPath = topicSettings.ResourceName;
 
@@ -31,11 +32,21 @@ namespace LagoVista.Core.Rpc.Tests.Server
             var message = new Rpc.Messages.Message
             {
                 OrganizationId = "c8ad4589f26842e7a1aefbaefc979c9b",
-                InstanceId = "9e88c7f6b5894dbfb3bc09d20736705e"
+                InstanceId = "9e88c7f6b5894dbfb3bc09d20736705e",
+                Id = Guid.NewGuid().ToId(),
+                CorrelationId = Constants.MessageCorrelationId,
+                DestinationPath = Constants.MessageDestination,
+                TimeStamp = Constants.MessageTimeStamp,
+                ReplyPath = Constants.MessageReplyPath
             };
 
             //rpc_request_c8ad4589f26842e7a1aefbaefc979c9b_9e88c7f6b5894dbfb3bc09d20736705e
-            var entityPath = (_destinationEntityPath + $"_{message.OrganizationId}_{message.InstanceId}").Replace("__", "_").ToLower();
+            var entityPath = $"{_destinationEntityPath}_{message.OrganizationId}_{message.InstanceId}"
+                .Replace("__", "_")
+                .ToLower();
+            Assert.AreEqual("rpc_request_c8ad4589f26842e7a1aefbaefc979c9b_9e88c7f6b5894dbfb3bc09d20736705e", entityPath);
+
+            //var entityPath = "lagovistatest";
             var topicClient = new TopicClient(_topicConnectionString, entityPath, RetryPolicy.Default);
 
             var messageOut = new Microsoft.Azure.ServiceBus.Message(message.Payload)
@@ -50,5 +61,6 @@ namespace LagoVista.Core.Rpc.Tests.Server
 
             await topicClient.SendAsync(messageOut);
         }
+#endif
     }
 }
