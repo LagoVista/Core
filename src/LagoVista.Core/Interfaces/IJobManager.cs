5b7d6485b0d9ad680704d67c990515fc894a95cd
Kevin D. Wolf <kevinw@software-logistics.com>
2021-02-17 18:58:07 -0500

    add support for job manager

---
+++ src/LagoVista.Core/Interfaces/IJobManager.cs
@@ -0,0 +1,21 @@
+﻿using LagoVista.Core.Models;
+using LagoVista.Core.Models.UIMetaData;
+using LagoVista.Core.Validation;
+using System.Threading.Tasks;
+
+namespace LagoVista.Core.Interfaces
+{
+    public interface IJobManager
+    {
+        Task<InvokeResult<string>> QueueJobAsync(Job job);
+        Task<ListResponse<Job>> GetJobsForJobTypeAsync(string jobTypeId, ListRequest request);
+        Task<Job> GetJobAsync(string jobId, string jobTypeId);
+        Task<string> GetJobPayloadAsync(string jobId, string jobTypeId);
+        Task<InvokeResult> MarkJobAsQueued(string jobId, string jobTypeId);
+        Task<InvokeResult> MarkJobAsStarted(string jobId, string jobTypeId);
+        Task<InvokeResult> UpdateJobProgress(string jobId, string jobTypeId, int percent);
+        Task<InvokeResult> MarkJobAsCompletedAsync(string jobId, string jobTypeId, string message = "");
+        Task<InvokeResult> MarkJobAsFailedAsync(string jobId, string jobTypeId, string error);
+
+    }
+}
---
+++ src/LagoVista.Core/Models/Job.cs
@@ -0,0 +1,77 @@
+﻿namespace LagoVista.Core.Models
+{
+    public enum JobStatus
+    {
+        New,
+        Queued,
+        Running,
+        Completed,
+        Error,
+    }
+
+    public class Job
+    {
+        /// <summary>
+        /// Unique ID for the Job, each job/process execution will have a unique job id
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Used to identify the job type id, this is usually specific to the job that will be picked up
+        /// the processor needs to know how to handle the job.
+        /// 
+        /// It will be specific to the processoor.
+        /// </summary>
+        public EntityHeader JobType { get; set; }
+        
+        /// <summary>
+        /// Name of the organization
+        /// </summary>
+        public EntityHeader Organization { get; set; }
+
+        /// <summary>
+        /// Procesor will be used to determine which queue will be used to process the job
+        /// </summary>
+        public EntityHeader Processor { get; set; }
+
+        /// <summary>
+        /// Progress Percent (optional, should mark as 100% when completed)
+        /// </summary>
+        public int PercentComplete { get; set; }
+
+        /// <summary>
+        // Current status of the job.
+        /// </summary>
+        public JobStatus Status { get; set; }
+
+        /// <summary>
+        /// Date stamp of when the job was queued, or empty if not queued.
+        /// </summary>
+        public string QueuedTimeStamp { get; set; }
+
+        /// <summary>
+        /// Timestamp of when the job was started.
+        /// </summary>
+        public string StartedTimeStamp { get; set; }
+
+        /// <summary>
+        /// Timestamp of when the job was completed.
+        /// </summary>
+        public string CompletedTimeStamp { get; set; }
+
+        /// <summary>
+        /// Total Execution Time for Job
+        /// </summary>
+        public double? ExecutionTimeSeconds { get; set; }
+
+        /// <summary>
+        /// Most recent error message.
+        /// </summary>
+        public string Error { get; set; }
+
+        /// <summary>
+        /// Payload for the job, will be stored in blob storage.
+        /// </summary>
+        public string Payload { get; set; }
+    }
+}
