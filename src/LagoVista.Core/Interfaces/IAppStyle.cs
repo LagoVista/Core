945f03090500cb1cf6906f19c43b07e63878b51a
Kevin D. Wolf <kevinw@software-logistics.com>
2021-07-19 07:31:45 -0400

    adjust styles

--- src/LagoVista.Core/Interfaces/IAppStyle.cs
+++ src/LagoVista.Core/Interfaces/IAppStyle.cs
@@ -1,70 +1,82 @@
 ﻿using LagoVista.Core.Models.Drawing;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace LagoVista.Core.Interfaces
 {
     public interface IAppStyle
     {
         Color TitleBarBackground { get; }
-        Color TitleBarText { get; }
-        Color SectionHeaderColor { get; }
-
+        Color TitleBarForeground { get; }
+       
         Color PageBackground { get; }
-        Color PageText { get; }
+        Color PageForeground { get; }
+        string PageTextFont { get; }
+        double PageTextFontSize { get; }
+
+        Color HighlightColor { get; }
+
+
+        Color HeaderColor { get; }
+        double HeaderFontSize { get; }
+        string HeaderFont { get; }
+
+
+        Color SubHeaderColor { get; }
+        double SubHeaderFontSize { get; }
+        string SubHeaderFont { get; }
+
 
         Color LabelText { get; }
-        Color EditControlBackground { get; }
-        Color EditControlText { get; }
+        string LabelFont { get; }
+        double LabelFontSize { get; }
 
-        Color EditControlFrame { get;  }
-        Color EditControlFrameFocus { get; }
-        Color EditControlFrameInvalid { get; }
+
+        Color EntryBackground { get; }
+        Color EntryForeground { get; }
+        Color EntryFrameColor { get;  }
+        Color EntryFrameColorFocus { get; }
+        Color EntryFrameColorFrameInvalid { get; }
+        string EntryFont { get; }
+        double EntryFontSize { get; }
+        double EntryMargin { get; }
+
 
         Color MenuBarBackground { get;  }
-
         Color MenuBarForeground { get; }
         Color MenuBarBackgroundActive { get; }
         Color MenuBarForegroundActive { get; }
+        double MenuFontSize { get; }
+        string MenuFont { get; }
+
 
         Color ButtonBackground { get; }
-        Color ButtonBorder { get; }
-        Color ButtonForeground { get; }
-            
-        Color ListItemColor { get; }
-        Color ListItemDetailColor { get; }
-        Color RowSeperatorColor { get; }
-
         Color ButtonBackgroundActive { get; }
+        Color ButtonBorder { get; }
         Color ButtonBorderActive { get; }
+        Color ButtonForeground { get; }
         Color ButtonForegroundActive { get; }
 
-        Color HighlightColor { get; }
-        
+        Color IconButtonForeground { get; }
+        Color IconButtonForegroundAcive { get; }
+        double IconButtonFontSize { get; }
+
+
+        string ListItemFont { get; }
+        string ListItemDetailFont { get; }
+        Color ListItemBackgroundColor { get; }
+        Color ListItemForegroundColor { get; }
+        double ListItemFontSize { get; }
+        double ListItemDetailFontSize { get; }
+        Color ListItemDetailForegroundColor { get; }
+        Color RowSeperatorColor { get; }
+       
 
         Color TabForground { get; }
         Color TabForgroundActive { get; }
         Color TabBackgroundActive { get; }
         Color TabBackground { get; }
         Color TabBarBackground { get; }
-
-
-        string HeaderFont { get; }
-        string ContentFont { get; }
-        string LabelFont { get; }
-        string EntryFont { get; }
-        string MenuFont { get; }
-        string ListItemFont { get; }
-        string TabBarFont { get; }
-
         double TabBarIconFontSize { get; }
         double TabBarFontSize { get; }
-        double HeaderFontSize { get; }
-        double LabelFontSize { get; }
-        double EntryFontSize { get; }
-        double ContentFontSize { get; }
-        double MenuFontSize { get; }
-        double ListItemFontSize { get; }
+        string TabBarFont { get; }
     }
 }
