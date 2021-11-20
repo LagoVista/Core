0d5868235dfd5026025431fb5fedd13ae5ee194a
Kevin D. Wolf <kevinw@software-logistics.com>
2021-11-20 08:58:57 -0500

    add support for labels

--- src/LagoVista.Core/Interfaces/ILabeledEntity.cs
+++ src/LagoVista.Core/Interfaces/ILabeledEntity.cs
@@ -3,8 +3,20 @@ using System.Collections.Generic;
 
 namespace LagoVista.Core.Interfaces
 {
+
+
     public interface ILabeledEntity: INoSQLEntity, IIDEntity, INamedEntity, IDescriptionEntity
     {
         List<Label> Labels { get; set; }
+    }
+
+    public class LabeledEntity : ILabeledEntity
+    {
+        public List<Label> Labels { get; set; }
+        public string DatabaseName { get; set; }
+        public string EntityType { get; set; }
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
     }
 }
