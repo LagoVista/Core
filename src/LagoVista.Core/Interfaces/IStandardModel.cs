adcc89157aae6eee42f3bc190039b4f271cc9179
Kevin Wolf <kevinw@software-logistics.com>
2023-10-25 09:36:45 -0400

    get any record by type and key.

---
+++ src/LagoVista.Core/Interfaces/IStandardModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LagoVista.Core.Interfaces
+{
+    public interface IStandardModel : IIDEntity, IOwnedEntity, INamedEntity, IKeyedEntity, INoSQLEntity
+    {
+    }
+}
--- src/LagoVista.Core/Interfaces/IStorageUtils.cs
+++ src/LagoVista.Core/Interfaces/IStorageUtils.cs
@@ -9,6 +9,7 @@ namespace LagoVista.Core.Interfaces
     {
         Task<TEntity> FindWithKeyAsync<TEntity>(string key, IEntityHeader org, bool throwOnNotFound = true) where TEntity : class, IIDEntity, INoSQLEntity, IKeyedEntity, IOwnedEntity;
         Task<TEntity> FindWithKeyAsync<TEntity>(string key) where TEntity : class, IIDEntity, INoSQLEntity, IKeyedEntity, IOwnedEntity;
+        Task<IStandardModel> FindWithKeyAsync(string objectType, string key, IEntityHeader org, bool throwOnNotFound = true);
         Task DeleteByKeyIfExistsAsync<TEntity>(string key, IEntityHeader org) where TEntity : class, IIDEntity, INoSQLEntity, IKeyedEntity, IOwnedEntity;
         Task DeleteAsync<TEntity>(string id, IEntityHeader org) where TEntity : class, IIDEntity, INoSQLEntity, IKeyedEntity, IOwnedEntity;
         Task UpsertDocumentAsync<TEntity>(TEntity obj) where TEntity : class, IIDEntity, INoSQLEntity, IKeyedEntity, IOwnedEntity;
