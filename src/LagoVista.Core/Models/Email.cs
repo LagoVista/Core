e8825fde1364b33aa957acc87af3ae24fbd56d5d
Kevin Wolf <kevinw@software-logistics.com>
2024-04-05 06:19:39 -0400

    add email object

---
+++ src/LagoVista.Core/Models/Email.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace LagoVista.Core.Models
+{
+    public class Email
+    {
+        public List<EmailAddress> To { get; set; }
+        public EmailAddress From {get; set;}
+        public EmailAddress ReplyTo { get; set; }
+
+        public string Subject { get; set; }
+        public string Content { get; set; }
+
+        public string TrackingLink { get; set; }
+     }
+
+    public class EmailAddress
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
