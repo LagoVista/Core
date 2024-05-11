f18f51ac0ff0c9f391276ee239daa3b7c2f890b8
Kevin D. Wolf <kevinw@software-logistics.com>
2024-05-11 18:34:17 -0400

    link shortner, static page save

---
+++ src/LagoVista.Core/Interfaces/ILinkShortner.cs
@@ -0,0 +1,10 @@
+﻿using LagoVista.Core.Validation;
+using System.Threading.Tasks;
+
+namespace LagoVista.Core.Interfaces
+{
+    public interface ILinkShortner
+    {
+        Task<InvokeResult<string>> ShortenLinkAsync(string url);
+    }
+}
---
+++ src/LagoVista.Core/Interfaces/IStaticPageStorage.cs
@@ -0,0 +1,14 @@
+﻿using LagoVista.Core.Validation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LagoVista.Core.Interfaces
+{
+    public interface IStaticPageStorage
+    {
+        Task<InvokeResult<string>> StorePageAsync(string htmlContent);
+        Task<InvokeResult<string>> GetPageAsync(string pageId);
+    }
+}
