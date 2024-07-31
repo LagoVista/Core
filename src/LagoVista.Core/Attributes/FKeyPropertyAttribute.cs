9b97eb70b9053f7593088dde20855047e2a66851
Kevin Wolf <kevinw@software-logistics.com>
2024-07-31 06:20:35 -0400

    fkey attribute add type as optional parameter

--- src/LagoVista.Core/Attributes/FKeyPropertyAttribute.cs
+++ src/LagoVista.Core/Attributes/FKeyPropertyAttribute.cs
@@ -9,16 +9,19 @@ namespace LagoVista.Core.Attributes
     {
 
 
-        public FKeyPropertyAttribute(string PKeyObjectName, string WhereClause = "", string UpdateClause = "")
+        public FKeyPropertyAttribute(string PKeyObjectName, Type Type = null, string WhereClause = "", string UpdateClause = "")
         {
             this.PKeyObjectName = PKeyObjectName;
             this.WhereClause = WhereClause;
             this.UpdateClause = UpdateClause;
+            this.FKeyType = Type;
         }
 
         public string PKeyObjectName { get;}
         public string WhereClause { get;}
 
         public string UpdateClause { get;}
+
+        public Type FKeyType { get; }
     }
 }
