ec3516bc592c7319431cb5ca3f0dd614b9728d0e
Kevin Wolf <kevinw@software-logistics.com>
2024-09-03 09:11:19 -0400

    add verbose logging

---
+++ src/LagoVista.Core/Interfaces/IRDBMSConnectionProvider.cs
@@ -0,0 +1,11 @@
+﻿using LagoVista.Core.Models;
+using System.Threading.Tasks;
+
+namespace LagoVista
+{
+    public interface IRDBMSConnectionProvider
+    {
+        Task<RDBMSConnection> GetConnectionAsync(string orgId);
+        Task MigrateSharedRDBMSAsync(string orgId, RDBMSConnectionType srcConnectionType, RDBMSConnectionType destConnection);
+    }
+}
---
+++ src/LagoVista.Core/Models/RDBMSConnection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LagoVista.Core.Models
+{
+    public enum RDBMSConnectionType
+    {
+        SQLServerShared,
+        SQLServerDedicated,
+        PosgreSQLDedicated
+    }
+
+    public class RDBMSConnection
+    {
+        public RDBMSConnection(RDBMSConnectionType connectionType, string connectionString)
+        {
+            ConnectionType = connectionType;
+            ConnectionString = connectionString;
+        }
+
+        public RDBMSConnection(RDBMSConnectionType connectionType)
+        {
+            ConnectionType = connectionType;
+        }
+
+        public RDBMSConnectionType ConnectionType { get; }
+        public string ConnectionString { get; }
+        
+        public bool VerboseLogging { get; set; }
+    }
+}
