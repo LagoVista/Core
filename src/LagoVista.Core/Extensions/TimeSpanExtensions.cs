530e8188b106a5ce569875c319ca6ea9e7614334
Kevin D. Wolf <kevinw@software-logistics.com>
2024-09-30 18:51:09 -0400

    add method to get intelligent string from timespan

---
+++ src/LagoVista.Core/Extensions/TimeSpanExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LagoVista
+{
+    public static class TimeSpanHelpers
+    {
+        public static string ToDescription(this TimeSpan timeSpanOverdue)
+        {
+            if (timeSpanOverdue.Days > 0)
+                return $"{timeSpanOverdue.Days}day(s), {timeSpanOverdue.Hours}hr(s) {timeSpanOverdue.Minutes}min(s)";
+            else if (timeSpanOverdue.Hours > 0)
+                return $"{timeSpanOverdue.Hours}hr(s) {timeSpanOverdue.Minutes}min(s)";
+            else if (timeSpanOverdue.Minutes > 0)
+                return $"{timeSpanOverdue.Minutes}min(s) {timeSpanOverdue.Seconds}second(s)";
+            else
+                return $"{timeSpanOverdue.Seconds}second(s)";
+        }
+    }
+}
