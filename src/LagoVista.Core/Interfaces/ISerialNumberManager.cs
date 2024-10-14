9542299e76b70bf191221f3a0f45a30422981784
Kevin D. Wolf <kevinw@software-logistics.com>
2024-10-14 18:55:56 -0400

    Add interface to generate serial numbers

---
+++ src/LagoVista.Core/Interfaces/ISerialNumberManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LagoVista.Core.Interfaces
+{
+    public interface ISerialNumberManager
+    {
+        Task<int> GenerateSerialNumber(string orgId, string key, string keyId = "NA", int seed = 1050);
+    }
+}
