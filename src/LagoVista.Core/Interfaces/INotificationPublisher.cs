ee713c16d8ab687afb5add4e1c33169f198824d5
Kevin Wolf <kevinw@software-logistics.com>
2025-01-21 13:10:47 -0600

    add notification publisher

---
+++ src/LagoVista.Core/Interfaces/INotificationPublisher.cs
@@ -0,0 +1,65 @@
+﻿using LagoVista.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LagoVista.Core.Interfaces
+{
+    public enum Channels
+    {
+        Org,
+        Team,
+        User,
+
+        Host,
+        Instance,
+        DeviceRepository,
+        DeviceGroup,
+        Device,
+        DeviceLog,
+        PipelineModule,
+        Route,
+        DeviceConfiguration,
+        MessageType,
+
+        Dependency,
+
+        Simulator,
+
+        Jobs,
+
+        DFU,
+
+        Customer,
+        ToDo,
+        WorkTask,
+        Inbox,
+
+        Entity,
+    }
+
+    public enum NotificationVerbosity
+    {
+        Diagnostics,
+        Normal,
+        Errors,
+    }
+
+    public enum Targets
+    {
+        Sms,
+        WebSocket,
+        Push,
+        WebSocketAndPush
+    }
+
+    public interface INotificationPublisher
+    {
+        Task PublishAsync(Targets target, Notification notification, NotificationVerbosity verbosity = NotificationVerbosity.Normal);
+        Task PublishAsync<TPayload>(Targets target, Channels channel, string channelId, TPayload message, NotificationVerbosity verbosity = NotificationVerbosity.Normal);
+        Task PublishAsync<TPayload>(Targets target, Channels channel, string channelId, String text, TPayload message, NotificationVerbosity verbosity = NotificationVerbosity.Normal);
+        Task PublishAsync<TEntity>(Targets target, TEntity entity, NotificationVerbosity verbosity = NotificationVerbosity.Normal) where TEntity : IIDEntity;
+        Task PublishTextAsync(Targets target, Channels channel, string channelId, String text, NotificationVerbosity verbosity = NotificationVerbosity.Normal);
+    }
+}
---
+++ src/LagoVista.Core/Models/Notification.cs
@@ -0,0 +1,43 @@
+﻿using LagoVista.Core.Interfaces;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LagoVista.Core.Models
+{
+    public class Notification
+    {
+        public Notification()
+        {
+            MessageId = Guid.NewGuid().ToId();
+            DateStamp = DateTime.Now.ToJSONString();
+        }
+
+        [JsonProperty("messageId")]
+        public String MessageId { get; set; }
+        [JsonProperty("dateStamp")]
+        public String DateStamp { get; set; }
+
+        [JsonProperty("channel")]
+        public EntityHeader<Channels> Channel { get; set; }
+
+        [JsonProperty("verbosity")]
+        public EntityHeader<NotificationVerbosity> Verbosity { get; set; }
+
+        [JsonProperty("channelId")]
+        public string ChannelId { get; set; }
+
+        [JsonProperty("title")]
+        public String Title { get; set; }
+
+        [JsonProperty("text")]
+        public String Text { get; set; }
+
+        [JsonProperty("payloadType")]
+        public String PayloadType { get; set; }
+
+        [JsonProperty("payloadJSON")]
+        public String Payload { get; set; }
+    }
+}
