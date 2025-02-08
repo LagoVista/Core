dfa01e50fcee7da3846cee5b3963066763598b2f
Kevin Wolf <kevinw@software-logistics.com>
2025-02-08 17:45:06 -0600

    add new interface for cusotmer owned entities

---
+++ src/LagoVista.Core/Interfaces/ICustomerOwneedEntity.cs
@@ -0,0 +1,12 @@
+﻿using LagoVista.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LagoVista.Core.Interfaces
+{
+    public interface ICustomerOwnedEntity
+    {
+        EntityHeader Customer { get; set; }
+    }
+}
