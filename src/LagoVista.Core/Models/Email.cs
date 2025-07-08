2e440c7ea7dea49ba53e8872edbaef1062135380
Kevin Wolf <kevinw@software-logistics.com>
2025-07-08 07:26:32 -0400

    support for email attachments

--- src/LagoVista.Core/Models/Email.cs
+++ src/LagoVista.Core/Models/Email.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Net.Http.Headers;
 
 namespace LagoVista.Core.Models
 {
@@ -6,14 +7,16 @@ namespace LagoVista.Core.Models
     {
         public List<EmailAddress> To { get; set; }
         public List<EmailAddress> Cc { get; set; }
-        public EmailAddress From {get; set;}
+        public EmailAddress From { get; set; }
         public EmailAddress ReplyTo { get; set; }
 
         public string Subject { get; set; }
         public string Content { get; set; }
 
         public string TrackingLink { get; set; }
-     
+
+        public List<EmailAttachment> Attachments { get; set; } = new List<EmailAttachment>();
+
         public EntityHeader Template { get; set; }
     }
 
@@ -22,4 +25,13 @@ namespace LagoVista.Core.Models
         public string Name { get; set; }
         public string Address { get; set; }
     }
+
+    public class EmailAttachment 
+    {
+        public string FileName { get; set; }
+        public string ContentType { get; set; }
+        public string Base64Content { get; set; }
+
+    }
+
 }
