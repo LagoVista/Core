081cf0e5d76331a32b4760d2218ee65fb17d7749
Kevin D. Wolf <kevinw@software-logistics.com>
2026-01-06 17:52:21 -0500

    fix .tag() to show current base class

--- src/LagoVista.Core/Exceptions/LogExtensions.cs
+++ src/LagoVista.Core/Exceptions/LogExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
 public static class LogExtensions
@@ -8,6 +10,48 @@ public static class LogExtensions
         [CallerMemberName] string memberName = "",
         [CallerLineNumber] int lineNumber = 0)
     {
-        return $"[{source.GetType().Name}__{memberName}] Line: {lineNumber}";
+        string typeName = source.GetType().Name;
+
+        try
+        {
+            var frameMethod = new StackFrame(1, false).GetMethod();
+            if (frameMethod != null)
+            {
+                typeName = GetLogicalDeclaringTypeName(frameMethod) ?? typeName;
+            }
+        }
+        catch
+        {
+            // never let logging break logging
+        }
+
+        return $"[{typeName}__{memberName}] Line: {lineNumber}";
+    }
+
+    private static string? GetLogicalDeclaringTypeName(MethodBase method)
+    {
+        // Normal case (non-async, non-generated)
+        var declaringType = method.DeclaringType;
+
+        // If we’re in an async state machine, declaringType will look like "<Foo>d__10"
+        // and the real parent is stored in AsyncStateMachineAttribute.
+        var asyncAttr = method.GetCustomAttribute<AsyncStateMachineAttribute>();
+        if (asyncAttr?.StateMachineType != null)
+        {
+            // The state machine is a nested type inside the real declaring type.
+            return asyncAttr.StateMachineType.DeclaringType?.Name
+                   ?? asyncAttr.StateMachineType.Name;
+        }
+
+        // If we’re *inside* MoveNext() of the generated state machine,
+        // method won’t have AsyncStateMachineAttribute. In that case:
+        if (declaringType != null && declaringType.IsNested
+            && declaringType.Name.StartsWith("<", StringComparison.Ordinal))
+        {
+            // Often nested inside the real declaring type
+            return declaringType.DeclaringType?.Name;
+        }
+
+        return declaringType?.Name;
     }
 }
