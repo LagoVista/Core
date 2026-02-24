08eccc206624315a6b65bc51ec9ceb749e27bfb9
Kevin D. Wolf <kevinw@software-logistics.com>
2026-02-23 19:20:44 -0500

    Org/AppUser Repos

---
+++ src/LagoVista.Core/Repos/IAppUserRelationalRepo.cs
@@ -0,0 +1,17 @@
+﻿using LagoVista.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LagoVista.Core.Repos
+{
+    public interface IAppUserRelationalRepo
+    {
+        Task AddAppUserAsync(AppUserDTO user);
+        Task<AppUserDTO> GetAppUserAsync(string id);
+        Task UpdateAppUserAsync(AppUserDTO user);
+        Task DeleteAppUserAsync(string id);
+    }
+}
---
+++ src/LagoVista.Core/Repos/IOrganizationRelationalRepo.cs
@@ -0,0 +1,17 @@
+﻿using LagoVista.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LagoVista.Core.Repos
+{
+    public interface IOrganizationRelationalRepo
+    {
+        Task AddOrganizationAsync(OrganizationDTO organization);
+        Task<OrganizationDTO> GetOrganizationAsync(string id);
+        Task UpdateOrganizationAsync(OrganizationDTO organization);
+        Task DeleteOrganizationAsync(string id);
+    }
+}
