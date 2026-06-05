39f50dbdebb39eb0db26f65f95b4412e0c474d67
Kevin Wolf <kevinw@software-logistics.com>
2026-06-04 21:15:29 -0400

    attribute for entity authoring

---
+++ src/LagoVista.Core/Attributes/EntityAuthoringContextAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LagoVista.Core.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+    public sealed class EntityAuthoringContextAttribute : Attribute
+    {
+        public EntityAuthoringContextAttribute(string context)
+        {
+            Context = context;
+        }
+
+        public string Context { get; }
+    }
+}
