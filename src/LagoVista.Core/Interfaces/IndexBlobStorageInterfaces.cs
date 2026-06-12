31482b41a6eb0703b1a348d0ddd2728438843f53
Kevin Wolf <kevinw@software-logistics.com>
2026-06-12 17:11:09 -0400

    interfaces to support blob summary storage

---
+++ src/LagoVista.Core/Interfaces/IndexBlobStorageInterfaces.cs
@@ -0,0 +1,23 @@
+﻿using LagoVista.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LagoVista.Core.Interfaces
+{
+    public interface IStorageIdProvider
+    {
+        NormalizedId32 Id { get; }
+    }
+
+    public interface IOwnedStorageRecord
+    {
+        EntityHeader OwnerOrganization { get; }
+    }
+
+    public interface ISummaryTableBuilder<TSummary>
+    {
+        TSummary CreateSummary();
+    }
+
+}
