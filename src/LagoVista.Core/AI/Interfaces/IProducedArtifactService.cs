9d58ee18544771c7fcfef7a3e9e483696827a69d
Kevin Wolf <kevinw@software-logistics.com>
2026-07-09 20:11:38 -0400

    new produced artifact service

---
+++ src/LagoVista.Core/AI/Interfaces/IProducedArtifactService.cs
@@ -0,0 +1,11 @@
+﻿using LagoVista.Core.Models;
+using LagoVista.Core.Validation;
+using System.Threading.Tasks;
+
+namespace LagoVista.Core.AI.Interfaces
+{
+    public interface IProducedArtifactService
+    {
+        Task<InvokeResult> CreateProducedArtifactsAsync(IEntityBase entity);
+    }
+}
