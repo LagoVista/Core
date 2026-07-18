38745b775607a05bd818e3fe3ffba7a2ef9467d6
Kevin Wolf <kevinw@software-logistics.com>
2026-07-18 10:46:17 -0400

    add core interface for billing event recorder

---
+++ src/LagoVista.Core/Interfaces/IBillingEventRecorder.cs
@@ -0,0 +1,29 @@
+﻿using LagoVista.Core.Models;
+using System.Threading.Tasks;
+
+namespace LagoVista.Core.Interfaces
+{
+    public enum BillingEventType
+    {
+        AudioGeneration,
+        VideoAvatarCreated,
+        VideoGeneration,
+        VideoAssembly
+    }
+
+    public interface IBillingEventRecorder
+    {
+        Task RecordUsageAsync(BillingEventType eventType, GuidString36 subscriptionId, double quantity, string note, EntityHeader org, EntityHeader user);
+        Task RecordUsageAsync(BillingEventType eventType, double quantity, string note, EntityHeader org, EntityHeader user);
+        Task RecordUsageAsync(BillingEventType eventType, double quantity, BillingUsageContext ctx);
+
+    }
+
+    public class BillingUsageContext
+    {
+        public GuidString36? subscriptionId { get; set; }
+        public EntityHeader Organization { get; set; }
+        public EntityHeader User { get; set; }
+        public string Note { get; set; }
+    }
+}
