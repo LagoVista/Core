b3689af45edf15181deccdf55575b3c3551a4dc5
Kevin D. Wolf <kevinw@software-logistics.com>
2026-08-23 13:34:18 -0400

    add version provider

---
+++ src/LagoVista.Core/Interfaces/IVersionProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LagoVista.Core.Interfaces
+{
+    public interface IVersionProvider
+    {
+        HostVersion GetHostVersion();
+    }
+
+    public class HostVersion
+    {
+        public string Version { get; set; }
+        public string BuildDate { get; set; }
+    }
+
+}
