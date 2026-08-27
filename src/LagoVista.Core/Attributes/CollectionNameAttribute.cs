8322979b8c115c0086de1539f9175ac6efafc0ac
Kevin Wolf <kevinw@software-logistics.com>
2026-08-26 21:42:33 -0400

    attributes

---
+++ src/LagoVista.Core/Attributes/CollectionNameAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LagoVista.Core
+{
+    public class CollectionNameAttribute : Attribute
+    {
+        public CollectionNameAttribute(string name) 
+        {
+            CollectionName = name;
+        }
+
+        public CollectionNameAttribute(string name, bool mongo, bool cosmos)
+        {
+            CollectionName = name;
+            Mongo = mongo;
+            Cosmos = cosmos;
+        }
+
+        public bool Mongo { get;  } = true;
+
+        public bool Cosmos { get; } = true;
+        public string CollectionName { get; }
+    }
+
+    public class CosmosPartitionKeyAttribute : Attribute
+    {
+        public CosmosPartitionKeyAttribute(string name)
+        {
+            PartitionKey = name;
+        }
+
+   
+
+        public string PartitionKey { get; }
+    }
+}
---
+++ src/LagoVista.Core/Attributes/StorageRecordName.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LagoVista.Core
+{
+    public class StorageRecordNameAttribute : Attribute
+    {
+
+        public StorageRecordNameAttribute(string name) 
+        {
+            StorageName = name;
+        }
+  
+        public string StorageName { get; }
+    }
+}
