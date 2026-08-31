5584364a1ec3e852493cfab09808b31ebe8de656
Kevin Wolf <kevinw@software-logistics.com>
2026-08-31 12:04:30 -0400

    logging on rabbit

--- src/LagoVista.MessageQueue.Rabbit/RabbitMqSubscriberHostedService.cs
+++ src/LagoVista.MessageQueue.Rabbit/RabbitMqSubscriberHostedService.cs
@@ -80,7 +80,7 @@ namespace LagoVista.MessageQueue.Rabbit
 
             try
             {
-                _logger.Trace($"[RabbitSubscriber__{nameof(RabbitMqSubscriberHostedService<TMessage>)}] starting '{_serviceName}'.", _serviceName.ToKVP("serviceName"), route.QueueName.ToKVP("queueName"), route.DestinationName.ToKVP("destinationName"), route.RouteKey.ToKVP("routeKey"));
+                _logger.Trace($"[RabbitSubscriber__{nameof(RabbitMqSubscriberHostedService<TMessage>)}] starting '{_serviceName}'.", _connectionFactory.HostName.ToKVP("hostname"), _connectionFactory.UserName.ToKVP("userName"),  _serviceName.ToKVP("serviceName"), route.QueueName.ToKVP("queueName"), route.DestinationName.ToKVP("destinationName"), route.RouteKey.ToKVP("routeKey"));
 
                 await EnsureConnectedAsync(route, stoppingToken).ConfigureAwait(false);
 
